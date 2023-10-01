85a818e167c76c178a947be85f2636dbd1cda620
Sebastian._.411._ <84807030+Debug41@users.noreply.github.com>
2023-10-01 17:57:26 -0500

    bases de datos

--- R/PrimeraEntrega/EsparanzaDeVida/diazR.R
+++ R/PrimeraEntrega/EsparanzaDeVida/diazR.R
@@ -1,17 +1,28 @@
 ## Parte de Diaz
 
-library(readxl)
-library(tidyverse)
-library(ggplot2)
-library(plotly)
-library(pastecs)
-library (fdth)
-library(PASWR2)
-library(descr)
-library(lmtest)
 library(dplyr)
 library(readr)
 
-life_expectancy <- read.csv("../baseDeDatos/lifeExpectancy.csv", header = TRUE, sep = ";") 
-problems(life_expectancy)
-spec()
+
+ruta_archivo <- "../baseDeDatos/lifeExpectancy.csv"
+life_expectancy <- read_csv2(ruta_archivo)
+
+# Convertir Year y Corruption en factores
+life_expectancy$Year <- as.factor(life_expectancy$Year)
+life_expectancy$Corruption <- as.factor(life_expectancy$Corruption)
+
+View(life_expectancy)
+
+regions <- c("East Asia & Pacific", "Europe & Central Asia", "Middle East & North Africa", "Sub-Saharan Africa")
+
+# Crear un nuevo dataframe solo con las poblaciones seleccionadas
+df_filtrado <- life_expectancy %>%
+  filter(Region %in% regions)
+
+# Calcular la estimación de la media para todas las variables en las poblaciones seleccionadas
+medias <- df_filtrado %>%
+  group_by(Region) %>%
+  summarise(across(where(is.numeric), mean, na.rm = TRUE))
+
+# Ver los resultados
+View(medias)
