9006b226a8c77e2ffc325a1cce8459d831d4bf39
Sebastian._.411._ <84807030+Debug41@users.noreply.github.com>
2023-10-01 20:20:54 -0500

    222

--- R/PrimeraEntrega/EsparanzaDeVida/diazR.R
+++ R/PrimeraEntrega/EsparanzaDeVida/diazR.R
@@ -1,28 +1,25 @@
-## Parte de Diaz
-
 library(dplyr)
 library(readr)
 
 
 ruta_archivo <- "../baseDeDatos/lifeExpectancy.csv"
-life_expectancy <- read_csv2(ruta_archivo)
 
-# Convertir Year y Corruption en factores
-life_expectancy$Year <- as.factor(life_expectancy$Year)
-life_expectancy$Corruption <- as.factor(life_expectancy$Corruption)
+life_expectancy <- read_csv2(ruta_archivo)
 
 View(life_expectancy)
 
+# Estimacion de muestra
+
+life_expectancy$Year <- as.factor(life_expectancy$Year)
+life_expectancy$Corruption <- as.factor(life_expectancy$Corruption)
+
 regions <- c("East Asia & Pacific", "Europe & Central Asia", "Middle East & North Africa", "Sub-Saharan Africa")
 
-# Crear un nuevo dataframe solo con las poblaciones seleccionadas
 df_filtrado <- life_expectancy %>%
   filter(Region %in% regions)
 
-# Calcular la estimación de la media para todas las variables en las poblaciones seleccionadas
 medias <- df_filtrado %>%
   group_by(Region) %>%
   summarise(across(where(is.numeric), mean, na.rm = TRUE))
 
-# Ver los resultados
 View(medias)
